b81bafaab3cc23625d3fb9b2eeb5ffc9da24751e
rusZovnet3 <cferls_ru@hotmail.com>
2022-11-04 01:03:34 -0400

    Maquetacion del modulo seleccionar.php  :::: seleccion de usuarios

---
+++ Vistas/modulos/seleccionar.php
@@ -0,0 +1,88 @@
+<section class="content">
+
+    <center>
+      <h1>Seleccione como desea Ingresar al Sistema</h1>
+    </center>
+
+      <div class="row">
+
+
+        <div class="col-lg-3 col-xs-6">
+
+          <!--Begin --- small box -->
+          <div class="small-box" style="background-color: #F781D8; color: white;">
+            <div class="inner">
+              <h3>Secretaria</h3>
+
+              <p>Iniciar Sesión</p>
+            </div>
+            <div class="icon">
+              <i class="fa fa-female"></i>
+            </div>
+            <a href="#" class="small-box-footer">Ingresar <i class="fa fa-arrow-circle-right"></i></a>
+          </div>
+          <!--End --- small box -->
+
+        </div>
+
+
+        <div class="col-lg-3 col-xs-6">
+
+          <!--Begin --- small box -->
+          <div class="small-box" style="background-color: #BDBDBD; color: white;">
+            <div class="inner">
+              <h3>Doctor</h3>
+
+              <p>Iniciar Sesión</p>
+            </div>
+            <div class="icon">
+              <i class="fa fa-user-md"></i>
+            </div>
+            <a href="#" class="small-box-footer">Ingresar <i class="fa fa-arrow-circle-right"></i></a>
+          </div>
+          <!--End --- small box -->
+
+        </div>
+
+
+        <div class="col-lg-3 col-xs-6">
+
+          <!--Begin --- small box -->
+          <div class="small-box bg-yellow">
+            <div class="inner">
+              <h3>Paciente</h3>
+
+              <p>Iniciar Sesión</p>
+            </div>
+            <div class="icon">
+              <i class="fa fa-users"></i>
+            </div>
+            <a href="#" class="small-box-footer">Ingresar <i class="fa fa-arrow-circle-right"></i></a>
+          </div>
+          <!--End --- small box -->
+
+        </div>
+
+
+        <div class="col-lg-3 col-xs-6">
+
+          <!--Begin --- small box -->
+          <div class="small-box bg-red">
+            <div class="inner">
+              <h3>Administrador</h3>
+
+              <p>Iniciar Sesión</p>
+            </div>
+            <div class="icon">
+              <i class="fa fa-male"></i>
+            </div>
+            <a href="#" class="small-box-footer">Ingresar <i class="fa fa-arrow-circle-right"></i></a>
+          </div>
+          <!--End --- small box -->
+
+        </div>
+
+
+      </div>
+
+    </section>
