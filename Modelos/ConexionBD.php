7b9bc10b204283ad8a7d5b0b068684ae296513bd
rusZovnet3 <cferls_ru@hotmail.com>
2022-11-04 01:08:55 -0400

    Conexion a la Base de Datos

---
+++ Modelos/ConexionBD.php
@@ -0,0 +1,8 @@
+<?php
+	class ConexionBD{
+		public function cBD(){
+			$bd = new PDO("mysql:host=localhost;dbname=websiteCitasMedicaOnline", "root", "localhost");
+			$bd->exec("set names utf8");
+			return $bd;
+		}
+	}
